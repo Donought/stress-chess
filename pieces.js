c0536a5359d6067fbd31529d007b6b9a48ad9d1f
Donought <alfred.baly@gmail.com>
2023-03-01 10:35:07 +0100

    Piece movement
    I've added a mousePressed function that makes it possible to use mouse input to move a piece. In this context I've also added a basic moveset to the Pawn, and I've added a section to drawBoard that shows the available moves for the piece that you click on.

--- pieces.js
+++ pieces.js
@@ -1,13 +1,31 @@
 class Base {
-  constructor(c, t) {
-    this.color = c; // 0 = black, 1 = white
-    this.type = t; // 0 - 5 (spritesheet)
-    this.sprite = sprites[t + c * 6];
-  }
+	constructor(c, t) {
+		this.color = c; // 0 = black, 1 = white
+		this.type = t; // 0 - 5 (spritesheet)
+		this.sprite = sprites[t + c * 6];
+		this.moves = []; // Available moves
+	}
 }
 
 class Pawn extends Base {
-  constructor(c) {
-    super(c, 3);
-  }
+	constructor(c) {
+		super(c, 3);
+	}
+
+	rules(x, y) {
+		let m = [];
+		// Basic rules
+		if (0 < this.color) {
+			m.push([x, y - 1]);
+			if (5 < y) {
+				m.push([x, y - 2]);
+			}
+		} else {
+			m.push([x, y + 1]);
+			if (y < 2) {
+				m.push([x, y + 2]);
+			}
+		}
+		this.moves = m;
+	}
 }
--- sketch.js
+++ sketch.js
@@ -2,123 +2,173 @@ let data;
 let sheet;
 let sprites = [];
 
-let board = []; // board, but that name was taken
+let board = []; // Contains an array that represents the board
 let offset = 0.85; // How much smaller the pieces are than the squares
 
+let boardSideLength;
+let squareSideLength;
+
+let actPiece = [];
+let actTiles = [];
+
 function preload() {
-  data = loadXML("spritesheet/data.xml");
-  sheet = loadImage("spritesheet/sheet.png");
+	data = loadXML("spritesheet/data.xml");
+	sheet = loadImage("spritesheet/sheet.png");
 }
 
 function setup() {
-  createCanvas(innerWidth, innerHeight);
+	createCanvas(innerWidth, innerHeight);
 
-  // Cut out sheet according to data and push cutouts to array
-  let children = data.getChildren("sprite");
-  for (let i = 0; i < children.length; i++) {
-    let x = children[i].getNum("x");
-    let y = children[i].getNum("y");
-    let w = children[i].getNum("w");
-    let h = children[i].getNum("h");
+	boardSideLength = height - (height / 20) * 2;
+	squareSideLength = boardSideLength / 8;
 
-    sprites.push(sheet.get(x, y, w, h));
-  }
+	// Cut out sheet according to data and push cutouts to array
+	let children = data.getChildren("sprite");
+	for (let i = 0; i < children.length; i++) {
+		let x = children[i].getNum("x");
+		let y = children[i].getNum("y");
+		let w = children[i].getNum("w");
+		let h = children[i].getNum("h");
 
-  newGame();
-  console.log(board);
+		sprites.push(sheet.get(x, y, w, h));
+	}
+
+	newGame();
+	console.log(board);
 }
 
 function draw() {
-  background(240);
-  drawBoard();
+	background(240);
+	drawBoard();
+}
+
+function mousePressed() {
+	let lever = 0;
+	let x = Math.floor(
+		(mouseX - (width - boardSideLength) / 2) / squareSideLength
+	);
+	let y = Math.floor(
+		(mouseY - (height - boardSideLength) / 2) / squareSideLength
+	);
+
+	actTiles.forEach(function (tile) {
+		if (tile[0] == x && tile[1] == y) {
+			let temp = board[actPiece[0]][actPiece[1]];
+			board[actPiece[0]].splice(actPiece[1], 1, "empty");
+			board[x].splice(y, 1, temp);
+			lever++;
+		}
+	});
+
+	actTiles = [];
+	actPiece = [];
+
+	if (x < 7 && y < 7 && 0 < x && 0 < y && lever == 0) {
+		let tile = board[x][y];
+		if (tile != "empty") {
+			actPiece = [x, y];
+			tile.rules(x, y);
+			actTiles = tile.moves;
+			console.log(actTiles);
+		}
+	}
 }
 
 // Function that creates the board array
 function newGame() {
-  // Creates board full of empty tiles
-  for (let i = 0; i < 8; i++) {
-    board.push([]);
-    for (let j = 0; j < 8; j++) {
-      board[i].push("empty");
-    }
-  }
+	// Creates board full of empty tiles
+	for (let i = 0; i < 8; i++) {
+		board.push([]);
+		for (let j = 0; j < 8; j++) {
+			board[i].push("empty");
+		}
+	}
 
-  // Adds a single white pawn on d2
-  board[3].splice(6, 1, new Pawn(1));
+	// Adds a single white pawn on d2
+	board[3].splice(1, 1, new Pawn(0));
 }
 
 function drawBoard() {
-  noStroke();
-  let boardSideLength = height - (height / 20) * 2;
-  let squareSideLength = boardSideLength / 8;
-  let r = 0;
-  u = true;
-  for (let j = 0; j <= 7; j++) {
-    let t = 0;
-    u = !u;
-    if (u == true) {
-      for (let i = 0; i <= 3; i++) {
-        //black color
-        fill(66, 48, 32);
-        square(
-          (width - boardSideLength) / 2 + squareSideLength * t,
-          (height - boardSideLength) / 2 + squareSideLength * r,
-          squareSideLength
-        );
-        t++;
-        //White color
-        fill(213, 176, 122);
-        square(
-          (width - boardSideLength) / 2 + squareSideLength * t,
-          (height - boardSideLength) / 2 + squareSideLength * r,
-          squareSideLength
-        );
-        t++;
-      }
-    } else {
-      for (let i = 0; i <= 3; i++) {
-        //White color
-        fill(213, 176, 122);
-        square(
-          (width - boardSideLength) / 2 + squareSideLength * t,
-          (height - boardSideLength) / 2 + squareSideLength * r,
-          squareSideLength
-        );
-        t++;
-        //black color
-        fill(66, 48, 32);
-        square(
-          (width - boardSideLength) / 2 + squareSideLength * t,
-          (height - boardSideLength) / 2 + squareSideLength * r,
-          squareSideLength
-        );
-        t++;
-      }
-    }
-  
-    r++;
-  }
+	noStroke();
+	let r = 0;
+	u = true;
+	for (let j = 0; j <= 7; j++) {
+		let t = 0;
+		u = !u;
+		if (u == true) {
+			for (let i = 0; i <= 3; i++) {
+				//black color
+				fill(66, 48, 32);
+				square(
+					(width - boardSideLength) / 2 + squareSideLength * t,
+					(height - boardSideLength) / 2 + squareSideLength * r,
+					squareSideLength
+				);
+				t++;
+				//White color
+				fill(213, 176, 122);
+				square(
+					(width - boardSideLength) / 2 + squareSideLength * t,
+					(height - boardSideLength) / 2 + squareSideLength * r,
+					squareSideLength
+				);
+				t++;
+			}
+		} else {
+			for (let i = 0; i <= 3; i++) {
+				//White color
+				fill(213, 176, 122);
+				square(
+					(width - boardSideLength) / 2 + squareSideLength * t,
+					(height - boardSideLength) / 2 + squareSideLength * r,
+					squareSideLength
+				);
+				t++;
+				//black color
+				fill(66, 48, 32);
+				square(
+					(width - boardSideLength) / 2 + squareSideLength * t,
+					(height - boardSideLength) / 2 + squareSideLength * r,
+					squareSideLength
+				);
+				t++;
+			}
+		}
 
-  // Add piece sprites
-  push();
-  translate(
-    (width - boardSideLength) / 2 + squareSideLength / 2,
-    (height - boardSideLength) / 2 + squareSideLength / 2
-  );
-  board.forEach(function (row, i) {
-    row.forEach(function (tile, j) {
-      if (tile != "empty") {
-        let temp = tile.sprite;
-        imageMode(CENTER);
-        image(
-          temp,
-          i * squareSideLength,
-          j * squareSideLength,
-          temp.width * (squareSideLength / temp.height) * offset,
-          squareSideLength * offset
-        );
-      }
-    });
-  });
-  pop();
+		r++;
+	}
+
+	// Add piece sprites
+	push();
+	translate(
+		(width - boardSideLength) / 2 + squareSideLength / 2,
+		(height - boardSideLength) / 2 + squareSideLength / 2
+	);
+	board.forEach(function (row, i) {
+		row.forEach(function (tile, j) {
+			if (tile != "empty") {
+				let temp = tile.sprite;
+				imageMode(CENTER);
+				image(
+					temp,
+					i * squareSideLength,
+					j * squareSideLength,
+					temp.width * (squareSideLength / temp.height) * offset,
+					squareSideLength * offset
+				);
+			}
+		});
+	});
+
+	// Draw available moves
+	translate(-squareSideLength / 2, -squareSideLength / 2);
+	fill(180, 220, 255, 255 * 0.5);
+	actTiles.forEach(function (tile) {
+		square(
+			tile[0] * squareSideLength,
+			tile[1] * squareSideLength,
+			squareSideLength
+		);
+	});
+	pop();
 }
