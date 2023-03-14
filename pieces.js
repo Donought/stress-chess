00408c6d09d2dfb643c68f1628e92f17dfc19331
EmmiPigen <73609450+EmmiPigen@users.noreply.github.com>
2023-03-14 09:54:49 +0100

    Moved king code to pieces

--- pieces.js
+++ pieces.js
@@ -71,5 +71,32 @@ rules(x, y) {
 	}
 }
 
+class King extends Base {
+	constructor(c) {
+		super(c, 1);
+	}
 
+	rules(x, y) {
+		let m = [];
+		// Basic rules
+        // 1 op 
+        m.push([x, y + 1]);
+        // 1 ned            
+        m.push([x, y - 1]); 
+        // 1 til højre   
+        m.push([x + 1, y]); 
+        // 1 til venstre    
+        m.push([x - 1, y]); 
+        // skråt op til venstre  
+        m.push([x + 1, y - 1]); 
+        // skråt op til højre    
+        m.push([x - 1, y - 1]);
+        // skråt ned til venstre  
+        m.push([x + 1, y + 1]); 
+        // skråt ned til højre    
+        m.push([x - 1, y + 1]);           
+        
+		this.moves = m;
+	}
+}
 
