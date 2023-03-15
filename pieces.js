c95acd494dab7ca28e612d72bff4a9bced8f9aba
BeyondConfused <114650102+BeyondConfused@users.noreply.github.com>
2023-03-15 11:04:55 +0100

    Fixed rook AGAIN

--- pieces.js
+++ pieces.js
@@ -38,37 +38,14 @@ class Rook extends Base {
 	rules(x, y) {
 		let m = [];
 		// Basic rules
-		if (0 == this.color) {
-			let temp = y;
-			// Up movement
-
-			for (let i = 0; i < 8; i++) {
-				{
-					m.push([x, y - 1]);
-
-					y = y - 1;
-				}
-			}
-
-			y = temp;
-			// Down movement
-			for (let i = 0; i < 8; i++) {
-				m.push([x, y + 1]);
-				y = y + 1;
-			}
-			y = temp;
+		for (let i = 0; i < 9; i++) {
+			//Top Left
+			m.push([x , y - i]);
+			m.push([x , y + i]);
+			m.push([x - i, y]);
+			m.push([x + i, y]);
 		}
-		// Left movement
-		let temp = x;
-		for (let i = 0; i < 8; i++) {
-			m.push([x - 1, y]);
-			x = x - 1;
-		}
-		x = temp;
-		for (let i = 0; i < 8; i++) {
-			m.push([x + 1, y]);
-			x = x + 1;
-		}
+
 		this.moves = m;
 	}
 }
@@ -153,5 +130,5 @@ class Knight extends Base {
 class Queen extends Base {
 	constructor(c) {
 		super(c, 4);
-	}	
+	}
 }
