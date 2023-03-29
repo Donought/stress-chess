033df00535c4a3ced62318c618de0cc203bb3f0b
Donought <alfred.baly@gmail.com>
2023-03-29 15:29:01 +0200

    Small change to functions in util.js

--- util.js
+++ util.js
@@ -1,15 +1,14 @@
 function arrayEqualityCheck(a, b) {
 	// Check for basic properties first
-	if (a === b) return true;
-	if (a == null || b == null) return false;
-	if (a.length !== b.length) return false;
+	if (a === b) return true; // If array is the exact same, return true
+	if (a.length !== b.length) return false; // If length is different, return false
 
-	// Check for quality for all index. If it finds an inequality, it returns false
+	// Check for equality for all index. If inequality is found, returns false
 	for (let i = 0; i < a.length; ++i) {
 		if (a[i] !== b[i]) return false;
 	}
 
-	return true;
+	return true; // If it passed the test, return true
 }
 
 function arrayInArrayOccurence(arr1, arr2) {
