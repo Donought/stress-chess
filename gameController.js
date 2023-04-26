dd6ef02ca90dca29361e16e73e3e8c248f07c75a
EmmiPigen <73609450+EmmiPigen@users.noreply.github.com>
2023-04-26 14:35:15 +0200

    formatting

--- gameController.js
+++ gameController.js
@@ -1,56 +1,56 @@
 class GameController {
-	constructor() {
-		this.turn = 1;
-		this.gameStopped = false;
-	}
-	winCheck() {
-		let win;
-		let aPieces = [];
-		let kings = [];
-		if (!this.gameStopped) {
-			for (let j = 0; j < 8; j++) {
-				for (let i = 0; i < 8; i++) {
-					// Places all pieces into a single array
-					if (board[i][j] != "empty") {
-						//console.log("Running")
-						aPieces.push(board[i][j]);
-					}
-				}
-			}
-			//console.log(aPieces)
-			for (let i = 0; i < aPieces.length; i++) {
-				if (aPieces[i].type == 1) {
-					kings.push(aPieces[i]);
-					//  console.log(kings)
-				}
-			}
+  constructor() {
+    this.turn = 1;
+    this.gameStopped = false;
+  }
+  winCheck() {
+    let win;
+    let aPieces = [];
+    let kings = [];
+    if (!this.gameStopped) {
+      for (let j = 0; j < 8; j++) {
+        for (let i = 0; i < 8; i++) {
+          // Places all pieces into a single array
+          if (board[i][j] != "empty") {
+            //console.log("Running")
+            aPieces.push(board[i][j]);
+          }
+        }
+      }
+      //console.log(aPieces)
+      for (let i = 0; i < aPieces.length; i++) {
+        if (aPieces[i].type == 1) {
+          kings.push(aPieces[i]);
+          //  console.log(kings)
+        }
+      }
 
-			if (aPieces.length < 3) {
-				alert("Det står lige, genindlæs side for at genstarte");
-				this.gameStopped = true;
-			}
-			if (kings.length == 1) {
-				if (kings[0].color == 0) {
-					win = "Sort spiller vinder";
-				} else {
-					win = "Hvid spiller vinder";
-				}
-				this.gameStopped = true;
-				//console.log(win)
-				alert(win + ", genindlæs side for at genstarte");
-				this.gameStopped = true;
-			}
-		}
-	}
+      if (aPieces.length < 3) {
+        alert("Det står lige, genindlæs side for at genstarte");
+        this.gameStopped = true;
+      }
+      if (kings.length == 1) {
+        if (kings[0].color == 0) {
+          win = "Sort spiller vinder";
+        } else {
+          win = "Hvid spiller vinder";
+        }
+        this.gameStopped = true;
+        //console.log(win)
+        alert(win + ", genindlæs side for at genstarte");
+        this.gameStopped = true;
+      }
+    }
+  }
 
-	playerText() {
-		let player;
-		if (this.turn == 1) {
-			player = "Hvid spillers";
-		} else {
-			player = "Sort spillers";
-		}
+  playerText() {
+    let player;
+    if (this.turn == 1) {
+      player = "Hvid spillers";
+    } else {
+      player = "Sort spillers";
+    }
 
-		this.playerTurn = player + " tur";
-	}
+    this.playerTurn = player + " tur";
+  }
 }
